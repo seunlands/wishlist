345919d653f9ca286fc71c1b060c6d00d3e732c3
Seun Landserg <seun.landsberg@gmail.com>
2013-07-22 21:46:12 +0200

    ajout classe personne

---
+++ webApp/wishlist-bean/src/main/java/org/landocore/wishlist/beans/Personne.java
@@ -0,0 +1,11 @@
+package org.landocore.wishlist.beans;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: seun
+ * Date: 22/07/13
+ * Time: 21:35
+ * To change this template use File | Settings | File Templates.
+ */
+public class Personne {
+}
