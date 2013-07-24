9b0d82d0c54d8c85862a81ff72d17cf5eb86b1fc
Seun Landserg <seun.landsberg@gmail.com>
2013-07-24 06:42:16 +0200

    continue archi

--- webApp/wishlist-bean/src/main/java/org/landocore/wishlist/beans/Personne.java
+++ webApp/wishlist-bean/src/main/java/org/landocore/wishlist/beans/Personne.java
@@ -1,5 +1,10 @@
 package org.landocore.wishlist.beans;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
 /**
  * Created with IntelliJ IDEA.
  * User: seun
@@ -7,5 +12,28 @@ package org.landocore.wishlist.beans;
  * Time: 21:35
  * To change this template use File | Settings | File Templates.
  */
-public class Personne {
+@Entity
+@Table(name="wl_personne")
+public class Personne implements Serializable{
+
+    @Id
+    private String email;
+
+    private String nom;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
 }
--- webApp/wishlist-web/src/main/java/org/landocore/web/action/index/IndexController.java
+++ webApp/wishlist-web/src/main/java/org/landocore/web/action/index/IndexController.java
@@ -1,5 +1,11 @@
 package org.landocore.web.action.index;
 
+import org.landocore.wishlist.beans.Personne;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * Created with IntelliJ IDEA.
  * User: seun
@@ -7,5 +13,11 @@ package org.landocore.web.action.index;
  * Time: 06:58
  * To change this template use File | Settings | File Templates.
  */
+@Controller
 public class IndexController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView indexHandler(){
+        return new ModelAndView("/index", "user", new Personne());
+    }
 }
